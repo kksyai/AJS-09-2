872b4ab30f6c82de762ed6e9305fe2b091bbba88
YanaRu <kksyai@gmail.com>
2023-01-26 18:25:14 +0300

    tests add

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import getBuffer from "./getBuffer";
-import ArrayBufferConverter from "./ArrayBufferConvert";
-
-const buffer = getBuffer();
-
-const test = new ArrayBufferConverter;
-
-console.log(test.load(buffer));
-console.log(test.toString(buffer));
